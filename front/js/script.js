8685dc757034a82f8e0f3f35dc8629bab0e25c8b
Laurie <laurie.voisin12@gmail.com>
2022-08-18 16:58:00 +0200

    Affichage des produits sur la page d'accueil

--- front/js/script.js
+++ front/js/script.js
@@ -1,40 +1,22 @@
 //Indication de l'emplacement du code injecté = section 'items'
-const sectionItems = document.querySelector('items');
+const sectionItems = document.getElementById("items");
 
 //Récupération des produits de l'API en format json
 fetch("http://localhost:3000/api/products")
     .then(response => response.json())
-    .then(apiProducts => productsList(apiProducts))
-    .catch(error => ("Erreur : " + error));
-
-function productsList(apiProducts) {
-    apiProducts.forEach((product) => {
-        console.table(product);
-
-    const itemsLink = 
-        document.createElement("a");
-        document.querySelector(".items");
-        itemsLink.appendChild(itemsLink);
-        itemsLink.href = `../html/product.html?id=${product._id}`;
-
-    const itemsArticle =
-        document.createElement("article");
-        itemsLink.appendChild(itemsArticle);
-
-    const itemsImg =
-        document.createElement("img");
-        itemsArticle.appendChild(itemsImg);
-        itemsImg.src = product.imgUrl;
-        itemsImg.alt = product.altText;
-
-    const itemsTitle =
-        document.createElement("h3");
-        itemsArticle.appendChild(itemsTitle);
-        itemsTitle.innerHTML = product.name;
-
-    const itemsDescription =
-        document.createElement("p");
-        itemsArticle.appendChild(itemsDescription);
-        itemsDescription.innerHTML = product.description;
-})}
+    .then(apiProducts => {
+//Injection sur la page des produits récupérés avec une boucle "for"
+        for(product of apiProducts) { 
+            sectionItems.innerHTML += 
+                `<a href="./product.html?id=${product.id}">
+                    <article>
+                        <img src=${product.imageUrl} alt=${product.altText}>
+                        <h3 class="productName">${product.name}</h3>
+                        <p classe="productDescription">${product.description}</p>
+                    </article>
+                </a>`
+        };
+    })
+//Retour d'un message s'il y a une erreur
+    .catch(error => "Erreur : " + error);
 
