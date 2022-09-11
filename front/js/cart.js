84430ebeb5b7f1f8973bcf139c5c961e578f9b1c
Laurie <laurie.voisin12@gmail.com>
2022-09-11 18:05:57 +0200

    Récupération des éléments du LS

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,44 +1,38 @@
 //Conversion chaîne JSON en objet JS + récupération des données du LocalStorage
 const productId = JSON.parse(localStorage.getItem("productResult"));
 
-console.log(productId);
-
 // Si : le panier (LocalStorage) est vide, affichage d'un message
 if (productId === null) {
     document.querySelector("h1").textContent = `Votre panier est vide.`;
-    document.getElementsByClassName("cart__price").style.display = "none";
-    document.getElementByClassName("cart__order").style.display = "none";
 }
 
 // Sinon : le panier contient des produits
 else {
     for (let cart of productId) {
-        //console.log(cart.selectedId);
-        //let id = productId.selectedProduct;
-        //console.log(productFinal);
-
-        fetch(`http://localhost:3000/api/products/${productId}`)
+        //Récuprération des informations des produits séléctionnés sur l'API
+        fetch(`http://localhost:3000/api/products`)
             .then(response => response.json())
-            .then(items => itemElement(items))
+            .then(items => itemElement(cart, items))
             .catch(error => "Erreur : " + error);
 
+        //Intégration des éléments dans le DOM
         function itemElement(items) {
             document.getElementById("cart__items").innerHTML +=
-            `<article class="cart__item" data-id=${items.slectedId
-            } data-color=${items.selectedColor}>
+            `<article class="cart__item" data-id=${items.selectedId} 
+            data-color=${items.selectedColor}>
                 <div class="cart__item__img">
-                    <img src=${productId.imageUrl} alt=${productId.altTxt}>
+                    <img src=${items.imageUrl} alt=${items.altTxt}>
                 </div>
                 <div class="cart__item__content">
                     <div class="cart__item__content__description">
-                        <h2>${productId.name}</h2>
+                        <h2>${items.name}</h2>
                         <p>${items.selectedColor}</p>
-                        <p>${productId.price}</p>
+                        <p>${items.price}</p>
                     </div>
                     <div class="cart__item__content__settings">
                         <div class="cart__item__content__settings__quantity">
                             <p>Qté : </p>
-                            <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
+                            <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${items.selectedQuantity}">
                         </div>
                         <div class="cart__item__content__settings__delete">
                             <p class="deleteItem">Supprimer</p>
@@ -46,7 +40,6 @@ else {
                     </div>
                 </div>
             </article>`
-
         }
     }
 }
